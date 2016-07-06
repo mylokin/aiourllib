106adab74a30aa9027cd930ad441ea0cd49a80ca
Andrey Gubarev <mylokin@me.com>
2016-07-06 04:32:13 +0300

    remove tests from uri module

--- aiourllib/uri.py
+++ aiourllib/uri.py
@@ -1,6 +1,5 @@
 import string
 import ipaddress
-import unittest
 
 
 class URIException(Exception):
@@ -327,43 +326,3 @@ def to_string(
     if fragment:
         uri_reference = '{}#{}'.format(uri_reference, fragment)
     return uri_reference
-
-
-class TestURI(unittest.TestCase):
-    def assertMatch(self, uri_reference):
-        self.assertEqual(
-            to_string(**from_string(uri_reference)), uri_reference)
-
-    def test_ftp(self):
-        self.assertMatch('ftp://ftp.is.co.za/rfc/rfc1808.txt')
-
-    def test_http(self):
-        self.assertMatch('http://www.ietf.org/rfc/rfc2396.txt')
-
-    def test_http_query_fragment(self):
-        self.assertMatch('http://www.ietf.org/rfc/rfc2396.txt?a#b')
-
-    def test_mailto(self):
-        self.assertMatch('mailto:John.Doe@example.com')
-
-    def test_news(self):
-        self.assertMatch('news:comp.infosystems.www.servers.unix')
-
-    def test_tel(self):
-        self.assertMatch('tel:+1-816-555-1212')
-
-    def test_telnet(self):
-        self.assertMatch('telnet://192.0.2.16:80/')
-
-    def test_urn(self):
-        self.assertMatch('urn:oasis:names:specification:docbook:dtd:xml:4.1.2')
-
-    # def test_abs_path(self):
-    #     self.assertMatch('file:///rfc3986.txt')
-
-    # def test_rel_path(self):
-    #     self.assertMatch('file://./rfc3986.txt')
-
-
-if __name__ == '__main__':
-    unittest.main()
