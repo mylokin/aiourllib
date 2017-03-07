dff9e5bd87f322cbb692a94e8ce9e389836f69e2
Andrey Gubarev <mylokin@me.com>
2017-03-07 13:52:57 +0300

    add protocol module

--- aiourllib/response.py
+++ aiourllib/response.py
@@ -1,83 +1,17 @@
 import asyncio
 import collections
 import json
-import re
 import gzip
 import zlib
 
 from . import (
     exc,
+    protocol,
     utils)
 
 
-class Protocol(object):
-    COLON = ':'
-    HTTP = 'HTTP/'
-
-    REGEX_CHARSET = re.compile(r';\s*charset=([^;]*)', re.I)
-    REGEX_TOKEN = re.compile(
-        r'([a-zA-Z][a-zA-Z_-]*)\s*(?:=(?:"([^"]*)"|'
-        r'([^ \t",;]*)))?')
-
-    @classmethod
-    def parse_status(cls, status):
-        if status.startswith(cls.HTTP):
-            http_version, status_code, status_text = status.split(None, 2)
-            status = '{} {}'.format(
-                utils.smart_text(status_code),
-                utils.smart_text(status_text))
-        return status
-
-    @classmethod
-    def parse_status_code(cls, status):
-        return int(status.split()[0])
-
-    @classmethod
-    def parse_charset(cls, header, charset):
-        match = cls.REGEX_CHARSET.search(utils.smart_text(header))
-        if match:
-            charset = match.group(1)
-        return charset
-
-    @classmethod
-    def parse_cache_control(cls, header):
-        header = utils.smart_text(header)
-        cache = {}
-        for match in cls.REGEX_TOKEN.finditer(header):
-            name = match.group(1)
-            value = match.group(2) or match.group(3) or None
-            if value and value.isdigit():
-                value = int(value)
-            cache[name] = value
-
-        cache_control = {}
-        for n in [
-            'public',
-            'no-store',
-            'no-transform',
-            'must-revalidate',
-            'proxy-revalidate',
-        ]:
-            if n not in cache:
-                continue
-            cache_control[n] = None
-
-        for n, v in cache.items():
-            if n not in [
-                'private',
-                'no-cache',
-                'max-age',
-                's-maxage',
-                'stale-while-revalidate',
-                'stale-if-error',
-            ]:
-                continue
-            cache_control[n] = v
-        return cache_control
-
-
 class Response(object):
-    PROTOCOL = Protocol
+    PROTOCOL = protocol.ResponseProtocol
 
     CONTENT_TYPE = 'text/html'
     CHARSET = 'UTF-8'
