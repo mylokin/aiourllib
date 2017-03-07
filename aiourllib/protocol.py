a60c3f69484b3685908558236c5b0f9186fd46f1
Andrey Gubarev <mylokin@me.com>
2017-03-07 14:02:03 +0300

    extract protocol

--- aiourllib/protocol.py
+++ aiourllib/protocol.py
@@ -1,4 +1,6 @@
 import re
+import string
+import ipaddress
 
 from . import utils
 
@@ -106,3 +108,198 @@ class ResponseProtocol(object):
                 continue
             cache_control[n] = v
         return cache_control
+
+
+class URIProtocol(object):
+    ALPHA = string.ascii_letters
+    DIGIT = string.digits
+    UNRESERVED = ALPHA + DIGIT + '-' '.' '_' '~'
+
+    GEN_DELIMS = ':' '/' '?' '#' '[' ']' '@'
+    SUB_DELIMS = '!' '$' '&' '\'' '(' ')' '*' '+' ',' ';' '='
+    RESERVED = GEN_DELIMS + SUB_DELIMS
+
+    PCT_ENCODED = '%' + string.hexdigits
+    PCHAR = UNRESERVED + PCT_ENCODED + SUB_DELIMS + ':' + '@'
+
+    # Parts
+    SCHEME = ALPHA + DIGIT + '+' + '-' + '.'
+    QUERY = PCHAR + '/' '?'
+    FRAGMENT = PCHAR + '/' '?'
+
+    # Authority
+    USERINFO = UNRESERVED + PCT_ENCODED + SUB_DELIMS + ':'
+    PORT = DIGIT
+
+    # Host
+    REG_NAME = UNRESERVED + PCT_ENCODED + SUB_DELIMS
+
+    # Path
+    SEGMENT = PCHAR
+    SEGMENT_NZ_NC = UNRESERVED + PCT_ENCODED + SUB_DELIMS + '@'
+
+    @classmethod
+    def strip_scheme(cls, uri):
+        if ':' not in uri:
+            return None, uri
+
+        scheme, hier_part = uri.split(':', 1)
+        if scheme[0] not in cls.ALPHA:
+            raise exc.SchemeException(uri)
+
+        if any(c not in cls.SCHEME for c in scheme[1:]):
+            raise exc.SchemeException(scheme)
+
+        return scheme.lower(), hier_part
+
+    @classmethod
+    def strip_fragment(cls, hier_part):
+        if '#' in hier_part:
+            hier_part, fragment = \
+                hier_part.rsplit('#', 1)
+            if any(c not in cls.FRAGMENT for c in fragment):
+                raise exc.FragmentException(fragment)
+        else:
+            fragment = None
+        return fragment, hier_part
+
+    @classmethod
+    def strip_query(cls, hier_part):
+        if '?' in hier_part:
+            hier_part, query = hier_part.rsplit('?', 1)
+            if any(c not in cls.QUERY for c in query):
+                raise exc.QueryException(query)
+        else:
+            query = None
+        return query, hier_part
+
+    @classmethod
+    def strip_authority(cls, hier_part):
+        if hier_part.startswith('//'):
+            hier_part = hier_part[2:]
+        if '/' in hier_part:
+            authority, hier_part = hier_part.split('/', 1)
+            hier_part = '/{}'.format(hier_part)
+        else:
+            authority = hier_part
+            hier_part = ''
+        return authority, hier_part
+
+    @classmethod
+    def strip_userinfo(cls, authority):
+        if '@' in authority:
+            userinfo, authority = authority.split('@', 1)
+            if any(c not in cls.USERINFO for c in userinfo):
+                raise exc.UserInfoException(userinfo)
+            if not userinfo:
+                userinfo = None
+        else:
+            userinfo = None
+        return userinfo, authority
+
+    @classmethod
+    def strip_port(cls, authority):
+        if ':' in authority:
+            authority, port = authority.rsplit(':', 1)
+        else:
+            return None, authority
+        if port.isdigit():
+            port = int(port)
+        else:
+            raise exc.PortException(port)
+        return port, authority
+
+    @classmethod
+    def verify_reg_name(cls, host):
+        return all(c in cls.REG_NAME for c in host)
+
+    @classmethod
+    def verify_ipv4_address(cls, host):
+        if '.' not in host:
+            return False
+
+        try:
+            ipaddress.IPv4Address(host)
+        except ipaddress.AddressValueError:
+            return False
+        else:
+            return True
+
+    @classmethod
+    def verify_ipv6_address(cls, host):
+        if ':' not in host:
+            return False
+
+        try:
+            ipaddress.IPv6Address(host)
+        except ipaddress.AddressValueError:
+            return False
+        else:
+            return True
+
+    @classmethod
+    def verify_path_abempty(cls, path):
+        if not path:
+            return True
+
+        if not path.startswith('/'):
+            return False
+
+        segments = path.split('/')
+        for segment in segments:
+            if any(c not in cls.SEGMENT for c in segment):
+                return False
+        else:
+            return True
+
+    @classmethod
+    def verify_path_absolute(cls, path):
+        if not path.startswith('/'):
+            return False
+
+        segments = path.split('/')[1:]
+        segment = segments[0]
+        if not segment or any(c not in cls.SEGMENT for c in segment):
+            return False
+
+        for segment in segments[1:]:
+            if any(c not in cls.SEGMENT for c in segment):
+                return False
+        else:
+            return True
+
+    @classmethod
+    def verify_path_noscheme(cls, path):
+        if path.startswith('/'):
+            return False
+
+        segments = path.split('/')
+        segment = segments[0]
+        if not segment or any(c not in cls.SEGMENT_NZ_NC for c in segment):
+            return False
+
+        for segment in segments[1:]:
+            if any(c not in cls.SEGMENT for c in segment):
+                return False
+        else:
+            return True
+
+    @classmethod
+    def verify_path_rootless(cls, path):
+        if path.startswith('/'):
+            return False
+
+        segments = path.split('/')
+        segment = segments[0]
+        if not segment or any(c not in cls.SEGMENT for c in segment):
+            return False
+
+        for segment in segments[1:]:
+            if any(c not in cls.SEGMENT for c in segment):
+                return False
+        else:
+            return True
+
+    @classmethod
+    def verify_path_empty(cls, path):
+        return bool(path)
--- aiourllib/uri.py
+++ aiourllib/uri.py
@@ -1,205 +1,9 @@
 import collections
-import string
-import ipaddress
 
 from . import (
     exc,
-    models)
-
-
-class Protocol(object):
-    ALPHA = string.ascii_letters
-    DIGIT = string.digits
-    UNRESERVED = ALPHA + DIGIT + '-' '.' '_' '~'
-
-    GEN_DELIMS = ':' '/' '?' '#' '[' ']' '@'
-    SUB_DELIMS = '!' '$' '&' '\'' '(' ')' '*' '+' ',' ';' '='
-    RESERVED = GEN_DELIMS + SUB_DELIMS
-
-    PCT_ENCODED = '%' + string.hexdigits
-    PCHAR = UNRESERVED + PCT_ENCODED + SUB_DELIMS + ':' + '@'
-
-    # Parts
-    SCHEME = ALPHA + DIGIT + '+' + '-' + '.'
-    QUERY = PCHAR + '/' '?'
-    FRAGMENT = PCHAR + '/' '?'
-
-    # Authority
-    USERINFO = UNRESERVED + PCT_ENCODED + SUB_DELIMS + ':'
-    PORT = DIGIT
-
-    # Host
-    REG_NAME = UNRESERVED + PCT_ENCODED + SUB_DELIMS
-
-    # Path
-    SEGMENT = PCHAR
-    SEGMENT_NZ_NC = UNRESERVED + PCT_ENCODED + SUB_DELIMS + '@'
-
-    @classmethod
-    def strip_scheme(cls, uri):
-        if ':' not in uri:
-            return None, uri
-
-        scheme, hier_part = uri.split(':', 1)
-        if scheme[0] not in cls.ALPHA:
-            raise exc.SchemeException(uri)
-
-        if any(c not in cls.SCHEME for c in scheme[1:]):
-            raise exc.SchemeException(scheme)
-
-        return scheme.lower(), hier_part
-
-    @classmethod
-    def strip_fragment(cls, hier_part):
-        if '#' in hier_part:
-            hier_part, fragment = \
-                hier_part.rsplit('#', 1)
-            if any(c not in Protocol.FRAGMENT for c in fragment):
-                raise exc.FragmentException(fragment)
-        else:
-            fragment = None
-        return fragment, hier_part
-
-    @classmethod
-    def strip_query(cls, hier_part):
-        if '?' in hier_part:
-            hier_part, query = hier_part.rsplit('?', 1)
-            if any(c not in Protocol.QUERY for c in query):
-                raise exc.QueryException(query)
-        else:
-            query = None
-        return query, hier_part
-
-    @classmethod
-    def strip_authority(cls, hier_part):
-        if hier_part.startswith('//'):
-            hier_part = hier_part[2:]
-        if '/' in hier_part:
-            authority, hier_part = hier_part.split('/', 1)
-            hier_part = '/{}'.format(hier_part)
-        else:
-            authority = hier_part
-            hier_part = ''
-        return authority, hier_part
-
-    @classmethod
-    def strip_userinfo(cls, authority):
-        if '@' in authority:
-            userinfo, authority = authority.split('@', 1)
-            if any(c not in cls.USERINFO for c in userinfo):
-                raise exc.UserInfoException(userinfo)
-            if not userinfo:
-                userinfo = None
-        else:
-            userinfo = None
-        return userinfo, authority
-
-    @classmethod
-    def strip_port(cls, authority):
-        if ':' in authority:
-            authority, port = authority.rsplit(':', 1)
-        else:
-            return None, authority
-        if port.isdigit():
-            port = int(port)
-        else:
-            raise exc.PortException(port)
-        return port, authority
-
-    @classmethod
-    def verify_reg_name(cls, host):
-        return all(c in cls.REG_NAME for c in host)
-
-    @classmethod
-    def verify_ipv4_address(cls, host):
-        if '.' not in host:
-            return False
-
-        try:
-            ipaddress.IPv4Address(host)
-        except ipaddress.AddressValueError:
-            return False
-        else:
-            return True
-
-    @classmethod
-    def verify_ipv6_address(cls, host):
-        if ':' not in host:
-            return False
-
-        try:
-            ipaddress.IPv6Address(host)
-        except ipaddress.AddressValueError:
-            return False
-        else:
-            return True
-
-    @classmethod
-    def verify_path_abempty(cls, path):
-        if not path:
-            return True
-
-        if not path.startswith('/'):
-            return False
-
-        segments = path.split('/')
-        for segment in segments:
-            if any(c not in cls.SEGMENT for c in segment):
-                return False
-        else:
-            return True
-
-    @classmethod
-    def verify_path_absolute(cls, path):
-        if not path.startswith('/'):
-            return False
-
-        segments = path.split('/')[1:]
-        segment = segments[0]
-        if not segment or any(c not in cls.SEGMENT for c in segment):
-            return False
-
-        for segment in segments[1:]:
-            if any(c not in cls.SEGMENT for c in segment):
-                return False
-        else:
-            return True
-
-    @classmethod
-    def verify_path_noscheme(cls, path):
-        if path.startswith('/'):
-            return False
-
-        segments = path.split('/')
-        segment = segments[0]
-        if not segment or any(c not in cls.SEGMENT_NZ_NC for c in segment):
-            return False
-
-        for segment in segments[1:]:
-            if any(c not in cls.SEGMENT for c in segment):
-                return False
-        else:
-            return True
-
-    @classmethod
-    def verify_path_rootless(cls, path):
-        if path.startswith('/'):
-            return False
-
-        segments = path.split('/')
-        segment = segments[0]
-        if not segment or any(c not in cls.SEGMENT for c in segment):
-            return False
-
-        for segment in segments[1:]:
-            if any(c not in cls.SEGMENT for c in segment):
-                return False
-        else:
-            return True
-
-    @classmethod
-    def verify_path_empty(cls, path):
-        return bool(path)
+    models,
+    protocol)
 
 
 def from_string(uri_reference):
@@ -225,39 +29,40 @@ def from_string(uri_reference):
         'relative_ref': None,
     }
 
-    uri['scheme'], hier_part = Protocol.strip_scheme(uri_reference)
-    uri['fragment'], hier_part = Protocol.strip_fragment(hier_part)
-    uri['query'], hier_part = Protocol.strip_query(hier_part)
+    uri['scheme'], hier_part = protocol.URIProtocol.strip_scheme(uri_reference)
+    uri['fragment'], hier_part = protocol.URIProtocol.strip_fragment(hier_part)
+    uri['query'], hier_part = protocol.URIProtocol.strip_query(hier_part)
     if uri['scheme']:
         # hier_part
         if hier_part.startswith('//'):
             # authority
-            authority, hier_part = Protocol.strip_authority(hier_part)
+            authority, hier_part = protocol.URIProtocol.strip_authority(
+                hier_part)
             uri['authority'] = authority
             components['userinfo'], authority = \
-                Protocol.strip_userinfo(authority)
+                protocol.URIProtocol.strip_userinfo(authority)
             components['port'], authority = \
-                Protocol.strip_port(authority)
+                protocol.URIProtocol.strip_port(authority)
             host = components['host'] = authority
-            if Protocol.verify_ipv6_address(host):
+            if protocol.URIProtocol.verify_ipv6_address(host):
                 components['ipv6_address'] = host
-            elif Protocol.verify_ipv4_address(host):
+            elif protocol.URIProtocol.verify_ipv4_address(host):
                 components['ipv4_address'] = host
-            elif Protocol.verify_reg_name(host):
+            elif protocol.URIProtocol.verify_reg_name(host):
                 components['reg_name'] = host
             else:
                 raise exc.AuthorityException(host)
 
-            if Protocol.verify_path_abempty(hier_part):
+            if protocol.URIProtocol.verify_path_abempty(hier_part):
                 uri['path'] = components['path_abempty'] = hier_part
             else:
                 raise exc.PathException(hier_part)
 
-        elif Protocol.verify_path_absolute(hier_part):
+        elif protocol.URIProtocol.verify_path_absolute(hier_part):
             uri['path'] = components['path_absolute'] = hier_part
-        elif Protocol.verify_path_rootless(hier_part):
+        elif protocol.URIProtocol.verify_path_rootless(hier_part):
             uri['path'] = components['path_rootless'] = hier_part
-        elif Protocol.verify_path_empty(hier_part):
+        elif protocol.URIProtocol.verify_path_empty(hier_part):
             uri['path'] = components['path_empty'] = hier_part
         else:
             raise exc.PathException(hier_part)
@@ -267,32 +72,33 @@ def from_string(uri_reference):
         relative_ref = components['relative_ref'] = hier_part
         if relative_ref.startswith('//'):
             # authority
-            authority, relative_ref = Protocol.strip_authority(relative_ref)
+            authority, relative_ref = protocol.URIProtocol.strip_authority(
+                relative_ref)
             uri['authority'] = authority
             components['userinfo'], authority = \
-                Protocol.strip_userinfo(authority)
+                protocol.URIProtocol.strip_userinfo(authority)
             components['port'], authority = \
-                Protocol.strip_port(authority)
+                protocol.URIProtocol.strip_port(authority)
             host = components['host'] = authority
-            if Protocol.verify_ipv6_address(host):
+            if protocol.URIProtocol.verify_ipv6_address(host):
                 components['ipv6_address'] = host
-            elif Protocol.verify_ipv4_address(host):
+            elif protocol.URIProtocol.verify_ipv4_address(host):
                 components['ipv4_address'] = host
-            elif Protocol.verify_reg_name(host):
+            elif protocol.URIProtocol.verify_reg_name(host):
                 components['reg_name'] = host
             else:
                 raise exc.AuthorityException(host)
 
-            if Protocol.verify_path_abempty(relative_ref):
+            if protocol.URIProtocol.verify_path_abempty(relative_ref):
                 uri['path'] = components['path_abempty'] = relative_ref
             else:
                 raise exc.PathException(relative_ref)
 
-        elif Protocol.verify_path_absolute(relative_ref):
+        elif protocol.URIProtocol.verify_path_absolute(relative_ref):
             uri['path'] = components['path_absolute'] = relative_ref
-        elif Protocol.verify_path_noscheme(relative_ref):
+        elif protocol.URIProtocol.verify_path_noscheme(relative_ref):
             uri['path'] = components['path_noscheme'] = relative_ref
-        elif Protocol.verify_path_empty(relative_ref):
+        elif protocol.URIProtocol.verify_path_empty(relative_ref):
             uri['path'] = components['path_empty'] = relative_ref
         else:
             raise exc.PathException(relative_ref)
